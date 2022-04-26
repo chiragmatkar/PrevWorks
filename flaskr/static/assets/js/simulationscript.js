80d65a7ea93f629d1855c4051adef57f0656d1ac
christen Clark <cnclark@usc.edu>
2022-04-26 13:11:09 -0700

    simulation update

--- flaskr/static/assets/js/simulationscript.js
+++ flaskr/static/assets/js/simulationscript.js
@@ -1,14 +1,18 @@
-// TODO: Account for floor plans
-// TODO: account for walls
-// TODO: account for ventilation
-// CDC has statistics that you can use
-
 function make2DArray(cols, rows) {
     let arr = new Array(cols);
     for(let i = 0; i < arr.length; i++) {
         arr[i] = new Array(rows);
     }
     return arr;
+}
+
+let dict = {
+    5: ["John Jones",1,"green"],
+    6: ["Erica Smith",3,"darkblue"],
+    7: ["Alice Young",1,"darkgreen"],
+    8: ["Donald White",3,"purple"],
+    9: ["Angela Lewis",1,"orange"],
+    10: ["Martin Strong",3,"blue"]
 }
 
 let grid;
@@ -151,18 +155,35 @@ function draw() {
                 unvaxxed_count++;
             }
             else if(grid[i][j] == 2) {
-                fill('lightgreen');
-                circle(x,y,res);
+                fill('red');
+                polygon(x, y, res/2, 4);
                 infected_count++;
             }
             else if(grid[i][j] == 3) {
-                fill('lightblue');
-                circle(x,y,res);
+                fill('tan');
+                //circle(x,y,res);
+                polygon(x, y, res/2, 6);
                 vaxxed_count++;
             }
             else if(grid[i][j] == 4) {
                 fill('grey');
                 square(x- (res/2), y - (res/2), res);
+            }
+            else if(grid[i][j] >= 5 && grid[i][j] <= 10) {
+                let idnum = grid[i][j];
+                fill(dict[idnum][2]);
+                if(dict[idnum][1] == 1) {
+                    circle(x,y,res);
+                    unvaxxed_count++;
+                }
+                else if(dict[idnum][1] == 2) {
+                    polygon(x, y, res/2, 4);
+                    infected_count++;
+                }
+                else if(dict[idnum][1] == 3) {
+                    polygon(x, y, res/2, 6);
+                    vaxxed_count++;
+                }
             }
         }
     }
@@ -199,10 +220,44 @@ function draw() {
                 let distance = Math.sqrt( Math.pow((x-mouseX),2) + Math.pow((y-mouseY),2) );
             
                 if(distance < res/2) {
-                    grid[i][j] = draw_mode;
-                    //noLoop();
-                    //redraw();
-                    break;
+                    if(draw_mode >= 0) {
+                        grid[i][j] = draw_mode;
+                        //noLoop();
+                        //redraw();
+                        break;
+                    }
+                    else if(draw_mode == -1 && grid[i][j] != 0 && grid[i][j] != 4) {
+                        document.querySelector("#select-box").style.display = "block";
+                        if(grid[i][j] == 1) {
+                            document.querySelector("#name").innerHTML = "Unknown";
+                            document.querySelector("#employee-id").innerHTML = "Unknown";
+                            document.querySelector("#status").innerHTML = "Unvaccinated";
+                        }
+                        else if(grid[i][j] == 2) {
+                            document.querySelector("#name").innerHTML = "Unknown";
+                            document.querySelector("#employee-id").innerHTML = "Unknown";
+                            document.querySelector("#status").innerHTML = "Infected";
+                        }
+                        else if(grid[i][j] == 3) {
+                            document.querySelector("#name").innerHTML = "Unknown";
+                            document.querySelector("#employee-id").innerHTML = "Unknown";
+                            document.querySelector("#status").innerHTML = "Vaccinated";
+                        }
+                        else if(grid[i][j] >= 5) {
+                            document.querySelector("#name").innerHTML = dict[grid[i][j]][0];
+                            document.querySelector("#employee-id").innerHTML = grid[i][j];
+                            if(dict[grid[i][j]][1] == 1) {
+                                document.querySelector("#status").innerHTML = "Unvaccinated";
+                            }
+                            else if(dict[grid[i][j]][1] == 2) {
+                                document.querySelector("#status").innerHTML = "Infected";
+                            }
+                            else if(dict[grid[i][j]][1] == 3) {
+                                document.querySelector("#status").innerHTML = "Vaccinated";
+                            }
+                        }
+                        break;
+                    }
                 }
             
             } 
@@ -227,38 +282,150 @@ function mouseReleased() {
     frameRate(1);
 }
 
-
+// TODO fix infection check for employees
 function infectionCheck(x, y) {
 
     let infectedNeighbors = 0;
 
-    for(let i = 0; i < 5; i++) {
-        for(let j = 0; j < 5; j++) {
-            
-            let neighborX = x - 2 + i;
-            let neighborY = y - 2 + j;
+    // North
+    let tempX = x;
+    let tempY = y;
+    for(let i = 0; i < 3; i++) {
 
-            if(neighborX < 0 || neighborX >= cols || neighborY < 0 || neighborY >= rows || (neighborX == x && neighborY == y)) {
-                continue;
-            }
-            else if(grid[neighborX][neighborY] == 2) {
-                infectedNeighbors++;
-            }
+        tempY++;
 
+        if(tempX < 0 || tempX >= cols || tempY < 0 || tempY >= rows || grid[tempX][tempY] == 4) {
+            break;
         }
+        else if(grid[tempX][tempY] == 2) {
+            infectedNeighbors++;
+        }
+    }
 
-        if(infectedNeighbors > 0) {
-            let rand = random(8);
-            if((grid[x][y] == 1 && rand < 4) || (grid[x][y] == 3 && rand < 2)) {
-                nextGrid[x][y] = 2;
-            }
-            else {
-                nextGrid[x][y] = grid[x][y];
-            }
+    // East
+    tempX = x;
+    tempY = y;
+    for(let i = 0; i < 3; i++) {
+
+        tempX++;
+
+        if(tempX < 0 || tempX >= cols || tempY < 0 || tempY >= rows || grid[tempX][tempY] == 4) {
+            break;
+        }
+        else if(grid[tempX][tempY] == 2) {
+            infectedNeighbors++;
+        }
+    }
+
+    // South
+    tempX = x;
+    tempY = y;
+    for(let i = 0; i < 3; i++) {
+
+        tempY--;
+
+        if(tempX < 0 || tempX >= cols || tempY < 0 || tempY >= rows || grid[tempX][tempY] == 4) {
+            break;
+        }
+        else if(grid[tempX][tempY] == 2) {
+            infectedNeighbors++;
+        }
+    }
+
+    // West
+    tempX = x;
+    tempY = y;
+    for(let i = 0; i < 3; i++) {
+
+        tempX--;
+
+        if(tempX < 0 || tempX >= cols || tempY < 0 || tempY >= rows || grid[tempX][tempY] == 4) {
+            break;
+        }
+        else if(grid[tempX][tempY] == 2) {
+            infectedNeighbors++;
+        }
+    }
+
+    // Northeast
+    tempX = x;
+    tempY = y;
+    for(let i = 0; i < 3; i++) {
+
+        tempY++;
+        tempX++;
+
+        if(tempX < 0 || tempX >= cols || tempY < 0 || tempY >= rows || grid[tempX][tempY] == 4) {
+            break;
+        }
+        else if(grid[tempX][tempY] == 2) {
+            infectedNeighbors++;
+        }
+    }
+
+    // Southeast
+    tempX = x;
+    tempY = y;
+    for(let i = 0; i < 3; i++) {
+
+        tempY--;
+        tempX++;
+
+        if(tempX < 0 || tempX >= cols || tempY < 0 || tempY >= rows || grid[tempX][tempY] == 4) {
+            break;
+        }
+        else if(grid[tempX][tempY] == 2) {
+            infectedNeighbors++;
+        }
+    }
+
+    // Southwest
+    tempX = x;
+    tempY = y;
+    for(let i = 0; i < 3; i++) {
+
+        tempY--;
+        tempX--;
+
+        if(tempX < 0 || tempX >= cols || tempY < 0 || tempY >= rows || grid[tempX][tempY] == 4) {
+            break;
+        }
+        else if(grid[tempX][tempY] == 2) {
+            infectedNeighbors++;
+        }
+    }
+
+    // Northwest
+    tempX = x;
+    tempY = y;
+    for(let i = 0; i < 3; i++) {
+
+        tempY++;
+        tempX--;
+
+        if(tempX < 0 || tempX >= cols || tempY < 0 || tempY >= rows || grid[tempX][tempY] == 4) {
+            break;
+        }
+        else if(grid[tempX][tempY] == 2) {
+            infectedNeighbors++;
+        }
+    }
+
+
+    if(infectedNeighbors > 0) {
+        let rand = random(8);
+        if((grid[x][y] == 1 && rand < 4) || (grid[x][y] == 3 && rand < 2)) {
+            nextGrid[x][y] = 2;
+        }
+        else if(grid[x][y] > 4 && ((dict[grid[x][y]][1] == 1 && rand < 4) || (dict[grid[x][y]][1] == 3 && rand < 2))) {
+            dict[grid[x][y]][1] = 2;
         }
         else {
             nextGrid[x][y] = grid[x][y];
         }
+    }
+    else {
+        nextGrid[x][y] = grid[x][y];
     }
 
 }
@@ -287,16 +454,52 @@ document.querySelector("#reset-btn").onclick = function() {
 
 document.querySelector("#wall-btn").onclick = function() {
     draw_mode = 4;
+    document.querySelector("#select-box").style.display = "none";    
 }
 document.querySelector("#infected-btn").onclick = function() {
     draw_mode = 2;
+    document.querySelector("#select-box").style.display = "none";
+
 }
 document.querySelector("#unvaxxed-btn").onclick = function() {
     draw_mode = 1;
+    document.querySelector("#select-box").style.display = "none";
+
 }
 document.querySelector("#vaxxed-btn").onclick = function() {
     draw_mode = 3;
+    document.querySelector("#select-box").style.display = "none";
+
 }
 document.querySelector("#erase-btn").onclick = function() {
     draw_mode = 0;
+    document.querySelector("#select-box").style.display = "none";
+
+}
+document.querySelector("#select-btn").onclick = function() {
+    draw_mode = -1;
+    //document.querySelector("#select-box").style.display = "block";
+}
+
+const employeeBtns = document.querySelectorAll('.btn-employee')
+
+employeeBtns.forEach(btn => {
+    btn.style.backgroundColor = dict[btn.dataset.id][2];
+    btn.onclick = function() {
+        console.log(this.dataset.id);
+        draw_mode = this.dataset.id;
+        document.querySelector("#select-box").style.display = "none";
+    }
+});
+
+
+function polygon(x, y, radius, npoints) {
+    let angle = TWO_PI / npoints;
+    beginShape();
+    for (let a = 0; a < TWO_PI; a += angle) {
+      let sx = x + cos(a) * radius;
+      let sy = y + sin(a) * radius;
+      vertex(sx, sy);
+    }
+    endShape(CLOSE);
 }
